05dfe6cf0a887d0e9dd5439b8896fb7b753563f7
sunil <sunil.sourashtriya143@gmail.com>
2025-07-11 12:13:36 +0530

    compose file changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
+
+# Remove old server PID file if exists (Rails doesn't like it)
 rm -f /docker_app/tmp/pids/server.pid
+
+# Run the Rails server with whatever CMD is passed
 exec "$@"
